fa3e9dd10255347e9d139b5df6e56aa93bf5d98c
Taturon <taturon5511@gmail.com>
2020-06-21 12:10:06 +0000

    Finish 100-knocks 022 Word Extraction

---
+++ lang_processing_100/ch03/022.php
@@ -0,0 +1,26 @@
+<?php
+// ファイルを開いて、各行ごとにデコード
+$lines = file('jawiki-country.json');
+foreach ($lines as $line) {
+	$data[] = json_decode($line);
+}
+
+// タイトルがイギリスの値となっているキーを取得
+foreach ($data as $key => $datum) {
+	if ($datum->title == 'イギリス') {
+		$i = $key;
+	}
+}
+
+// 取得したキーにより本文を抽出し各行毎に配列に格納
+$text = $data[$i]->text;
+$contents = explode("\n", $text);
+
+// 文字列内にcategoryが含まれている行をトリミングして配列に格納
+foreach ($contents as $content) {
+	if (strpos($content, 'Category') !== false) {
+		$categories[] = rtrim(mb_substr($content, 11), ']');
+	}
+}
+
+var_dump($categories);
